67163977c9808605cc80ef4ea0ee355a267237d5
kamal <habijabi@gmail.com>
2016-12-02 21:09:07 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+
+// });
+//     smoothScroll.init();   
+
+//    <script type="text/javascript">
+//   $('.timer').countTo();
+// </script>        
+
+// });
+
+$(document).ready(function() {
+  $(function(){
+        $('#menu').slicknav();
+    });
+
+
+      $('.timer').countTo();  
+      
+       $.scrollUp();  
+
+       new WOW().init();  
+
+       alert("Submitted");   
+
+});
+
+new WOW().init();
